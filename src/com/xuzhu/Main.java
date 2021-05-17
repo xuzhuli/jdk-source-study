a979d8a79ded9f0bda3005d174f03563b5c3adf7
liyunfeng <youyunpyy@163.com>
2021-05-17 21:05:14 +0800

    first commit

---
+++ src/com/xuzhu/Main.java
@@ -0,0 +1,14 @@
+package com.xuzhu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "b");
+    }
+}
